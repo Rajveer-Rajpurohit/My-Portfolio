2235d5c431c6fc45ed599d66b6cb4988b54662b5
Rajveer-Rajpurohit <rajveerrajpurohit186@gmail.com>
2026-04-16 18:45:00 +0530

    Finalized the Education Page

---
+++ cert.js
@@ -0,0 +1,17 @@
+const cards = document.querySelectorAll(".card");
+const desc = document.getElementById("cert-desc");
+let index = 0;
+
+function updateCarousel(){
+    cards.forEach(card => card.classList.remove("active"));
+    cards[index].classList.add("active");
+    desc.textContent = cards[index].dataset.desc;
+}
+document.getElementById("next").onclick = () => {
+    index = (index+1)%cards.length;
+    updateCarousel();
+};
+document.getElementById("prev").onclick = () => {
+    index = (index - 1 + cards.length)%cards.length;
+    updateCarousel();
+};
